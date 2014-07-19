afba84601a88203bed64f71988b74d17dffe332a
Michael Gans <michaelgans19@gmail.com>
2014-07-19 16:30:22 -0400

    new

--- Main copy.java
+++ Main copy.java
@@ -7,7 +7,7 @@ public class Main {
 		Integer numberOfChats=-1;
 		Integer numberOfMessages=0;
 		Integer numberOfWords=0;
-		TreeMap<String,Integer> words=new TreeMap<String,Integer>();
+		HashMap<String,Integer> words=new HashMap<String,Integer>();
 		File folder = new File("/Users/Michael/Documents/Developer/gws-emulator/server/chats");
 		File chat = new File("");
 		File[] listOfFiles = folder.listFiles();
@@ -40,33 +40,56 @@ public class Main {
 	        }
 		}
 		for(int i=0;i<numberOfMessages;i++){
-			String wordIndex[] = new String[1000];
-			wordIndex=messages[i].split("[\\[\\] \n\t\r.,;:!?(){}]");
-			for(int j=0;j<wordIndex.length;j++){
-				//System.out.println(wordIndex[j]);
-				String key=wordIndex[j].toLowerCase();
-				if(words.get(key)==null){
-					words.put(key,1);
-				}
-				else{
-					int value=words.get(key);
-					value++;
-					words.put(key,value);
+			System.out.println(messages[i]);
+			String key=messages[i].replace(" ","");
+			key="#"+key;
+			if(words.get(key)==null){
+				words.put(key,1);
+			}
+			else{
+				int value=words.get(key);
+				value++;
+				words.put(key,value);
+			}
+		}
+		//System.out.println(words);
+		File fileForSite = new File("/Users/Michael/Desktop/Hackathon/fileForSite.txt");
+		PrintWriter output = new PrintWriter(fileForSite);
+		Object [] valueHolder = words.values().toArray();
+		Integer [] integerHolder= new Integer[valueHolder.length];
+		for(int i=0;i<valueHolder.length;i++){
+			integerHolder[i]=(Integer) valueHolder[i];
+		}
+		Object [] namesHolder=words.keySet().toArray();
+		//System.out.println(valueHolder[1]);
+		//System.out.println(namesHolder[1]);
+		String Print=null;
+		for(int j=0;j<words.size();j++){
+			for(int i=0;i<words.size()-1;i++){
+				if(integerHolder[i]<integerHolder[i+1]){
+					int swap=integerHolder[i+1];
+					Object swap1=namesHolder[i+1];
+					integerHolder[i+1]=integerHolder[i];
+					namesHolder[i+1]=namesHolder[i];
+					namesHolder[i]=swap1;
+					integerHolder[i]=swap;
 				}
 			}
 		}
-		File fileForSite = new File("/Users/Michael/Documents/Developer/fileForSite.txt");
-		PrintWriter output = new PrintWriter(fileForSite);
-		for(int i=1;i<=numberOfChats;i++){
-			output.println("Chat number "+ i+":");
-			output.println(chats[i]);
+		for(int i=0;i<namesHolder.length;i++){
+			if(Print==null){
+				Print=namesHolder[i].toString();
+			}
+			else{
+			Print+=" "+namesHolder[i].toString();
+			}
 		}
-		output.println("\nTrends");
-		String holder = words.toString();
-		String [] holderArray=holder.split("[\\[\\] \n\t\r.,;:!?(){}]");
-		for(int i=0;i<holderArray.length;i++){
-			output.println(holderArray[i]);
-		}
+		output.println(Print);
+//		output.println("\n\n\n");
+//		for(int i=1;i<=numberOfChats;i++){
+//			output.println("Chat number "+ i+":");
+//			output.println(chats[i]);
+//		}
 		output.close();
 	}
 }
